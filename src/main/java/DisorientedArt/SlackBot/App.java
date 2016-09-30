a3407f825ecfc6b93c73c3b01b94e0b019210db2
DisorientedArt <disorientedart@gmail.com>
2016-09-29 20:07:55 -0500

    removed delete message

--- src/main/java/DisorientedArt/SlackBot/App.java
+++ src/main/java/DisorientedArt/SlackBot/App.java
@@ -16,28 +16,28 @@ public class App
     	SlackInfo.setToken(SlackInfo.externalToken());
     	
     	//Runs the code throughout a typical work day
-//    	APICalls.apiTest();
-//    	APICalls.authTest();
-//    	APICalls.setActiveUser();
-//    	APICalls.giphyMessage(Messages.randomAdjective());
-//    	//APICalls.postMessage("Repo Location : https://github.com/Disorientedart/SlackBot");
-//    	do{
-//    		try {
-//				APICalls.channelHistory();
-//			} catch (Exception e) {
-//				 //TODO Auto-generated catch block
-//				e.printStackTrace();
-//				break;
-//			}
-//    		Thread.sleep(2000);
-//    	}while(LocalTime.now().isBefore(LocalTime.of(18, 00)));
+    	APICalls.apiTest();
+    	APICalls.authTest();
+    	APICalls.setActiveUser();
+    	APICalls.giphyMessage(Messages.randomAdjective());
+    	//APICalls.postMessage("Repo Location : https://github.com/Disorientedart/SlackBot");
+    	do{
+    		try {
+				APICalls.channelHistory();
+			} catch (Exception e) {
+				 //TODO Auto-generated catch block
+				e.printStackTrace();
+				break;
+			}
+    		Thread.sleep(2000);
+    	}while(LocalTime.now().isBefore(LocalTime.of(18, 00)));
 
     	//Can be Used to remove any messages that are posted during the editing process of the code
     	//Add the Time Stamp of the message you want to delete,
     	//Time Stamp can be found in the console log of the previous run
     	
-    	SlackInfo.setDeleteMessage("1474953710.000012");
-    	APICalls.removeMessage();
+//    	SlackInfo.setDeleteMessage("1474953710.000012");
+//    	APICalls.removeMessage();
     	
     }
         
